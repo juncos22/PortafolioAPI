08a5cd872b5bbe53ebd98e8f4c0b5dab1b3434c1
juncos22 <juncosnicolas36@gmail.com>
2021-04-02 16:42:38 -0300

    Empty Commit

--- src/main/java/dev/nicolas/portafolioapi/config/ResourceConfig.java
+++ src/main/java/dev/nicolas/portafolioapi/config/ResourceConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/uploads/**").addResourceLocations("file:///app/uploads/");
     }
 }
